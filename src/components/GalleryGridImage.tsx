912622d399e5514d2fd4e0436a92b1250c6e17fd
danmlarsen <larsendanmarius@gmail.com>
2024-11-18 18:40:02 +0100

    HTML fixes

--- src/components/GalleryGridImage.tsx
+++ src/components/GalleryGridImage.tsx
@@ -92,25 +92,26 @@ export default function GalleryGridImage({
       className={`col-span-1 pb-10 ${colStart ? `${colStart}` : ""}`}
       style={{ gridRowEnd: `span ${rowSpan}` }}
     >
-      <figure
-        className="group relative h-full cursor-pointer overflow-hidden transition-shadow duration-300 focus:text-black focus:outline-none focus:ring-black focus-visible:ring-2 focus-visible:ring-offset-4"
+      <a
         onClick={handleClick}
         onKeyDown={handleKeydown}
         tabIndex={0}
-        role="link"
+        className="block h-full cursor-pointer transition-shadow duration-300 focus:text-black focus:outline-none focus:ring-black focus-visible:ring-2 focus-visible:ring-offset-4"
       >
-        <div className="absolute -inset-1 z-10 bg-white/50 opacity-0 transition-opacity duration-300 group-hover:opacity-100"></div>
-        <img
-          className="h-full object-cover transition duration-300 group-hover:scale-110"
-          src={image.images.thumbnail}
-          alt={`Thumbnail of image: ${image.name}`}
-          onLoad={handleImageLoaded}
-        />
-        <figcaption className="absolute bottom-0 left-0 right-0 z-20 flex h-[10.625rem] flex-col justify-end gap-[0.375rem] bg-gradient-to-t from-black/85 px-8 pb-8 text-white">
-          <h2 className="text-heading2">{image.name}</h2>
-          <p className="text-subhead2 text-white/75">{image.artist.name}</p>
-        </figcaption>
-      </figure>
+        <figure className="group relative h-full overflow-hidden">
+          <div className="absolute -inset-1 z-10 bg-white/50 opacity-0 transition-opacity duration-300 group-hover:opacity-100"></div>
+          <img
+            className="h-full object-cover transition duration-300 group-hover:scale-110"
+            src={image.images.thumbnail}
+            alt={`Thumbnail of image: ${image.name}`}
+            onLoad={handleImageLoaded}
+          />
+          <figcaption className="absolute bottom-0 left-0 right-0 z-20 flex h-[10.625rem] flex-col justify-end gap-[0.375rem] bg-gradient-to-t from-black/85 px-8 pb-8 text-white">
+            <h2 className="text-heading2">{image.name}</h2>
+            <p className="text-subhead2 text-white/75">{image.artist.name}</p>
+          </figcaption>
+        </figure>
+      </a>
     </motion.div>
   );
 }
